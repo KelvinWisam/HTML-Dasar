9b8fc58b7c20a0a4b7b9f199ba616f7ba3d81f0a
KelvinwisamGIT <144086301+KelvinwisamGIT@users.noreply.github.com>
2023-09-11 13:34:46 +0700

    Add files via upload

---
+++ Asset/Pages/js/dasar.js
@@ -0,0 +1,19 @@
+console.log("Belajar JS"); alert("Belajar JS");
+
+console.log("Belajar JS")
+alert("Belajar JS")
+var nama = "Kelvin Wisam Syahputra";
+let id = 123;
+const alamat = "Malang";
+let n =21;
+n= 30
+let inf = (100/0);
+console.log(inf);
+console.log("Arkatama + 1")
+console.log("Multi Solusindo" + (2*5));
+let boy = null;
+let employee = Symbol('Kelvin Wisam Syahputra');
+console.log(employee);
+console.log(employee.description);
+
+
